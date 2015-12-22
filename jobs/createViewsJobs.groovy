b21e8220cb34b11adc5d7d512b479befb6811bf6
Chernov Vasilii <v.chernov@crystals.ru>
2015-12-22 18:26:42 +0300

    [fix] removed views job

--- jobs/createViewsJobs.groovy
+++ jobs/createViewsJobs.groovy
@@ -1,27 +1,27 @@
-package com.setdsl
-
-import javaposse.jobdsl.dsl.views.jobfilter.Status
-
-listView('Flex') {
-    description('All Flex jobs')
-    filterBuildQueue()
-    filterExecutors()
-    jobs {
-        regex('build_flex')
-    }
-    jobFilters {
-        status {
-            status(Status.UNSTABLE)
-        }
-    }
-    columns {
-        status()
-        weather()
-        name()
-        lastSuccess()
-        lastFailure()
-        lastDuration()
-        buildButton()
-    }
-}
-
+//package com.setdsl
+//
+//import javaposse.jobdsl.dsl.views.jobfilter.Status
+//
+//listView('Flex') {
+//    description('All Flex jobs')
+//    filterBuildQueue()
+//    filterExecutors()
+//    jobs {
+//        regex('build_flex')
+//    }
+//    jobFilters {
+//        status {
+//            status(Status.UNSTABLE)
+//        }
+//    }
+//    columns {
+//        status()
+//        weather()
+//        name()
+//        lastSuccess()
+//        lastFailure()
+//        lastDuration()
+//        buildButton()
+//    }
+//}
+//
