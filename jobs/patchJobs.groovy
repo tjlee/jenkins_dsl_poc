ce6ac83fde6b6da718abc8b32175bd367c3eacd5
Chernov Vasilii <v.chernov@crystals.ru>
2016-03-01 15:06:57 +0300

    rt

--- jobs/patchJobs.groovy
+++ jobs/patchJobs.groovy
@@ -1,6 +1,6 @@
-//import com.setdsl.CashPatchBuilderMultiJobTemplate
-//import com.setdsl.ServerPatchBuilderMultiJobTemplate
-//
+import com.setdsl.CashPatchBuilderMultiJobTemplate
+import com.setdsl.ServerPatchBuilderMultiJobTemplate
+
 ///*
 //PARAMETERS
 // define git from
@@ -24,14 +24,14 @@
 //
 //// todo: define how to hide wrapper jobs
 //
-//new ServerPatchBuilderMultiJobTemplate(
-//        name: "build_server_patch",
-//        description: "Building server patch",
-//        isToRunTests: false
-//).build(this)
-//
-//new CashPatchBuilderMultiJobTemplate(
-//        name: "build_cash_patch",
-//        description: "Building cash patch",
-//        isToRunTests: false
-//).build(this)
+new ServerPatchBuilderMultiJobTemplate(
+        name: "build_server_patch",
+        description: "Building server patch",
+        isToRunTests: false
+).build(this)
+
+new CashPatchBuilderMultiJobTemplate(
+        name: "build_cash_patch",
+        description: "Building cash patch",
+        isToRunTests: false
+).build(this)
